fef7f314a8affe936667d7bd0380f18ec9af92ce
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-08-23 16:11:00 +0900

    サンプルの追加

---
+++ Sample/Def.fs
@@ -0,0 +1,18 @@
+﻿module Def
+
+open RealWorlds.ExcelHouganshi.TypeProvider.Data
+
+[<HouganshiDefinition>]
+let myHouganshi =
+  [
+    Define.field "Title" {
+      Type = typeof<string>
+      Sheet = "Sheet1"
+      Address = "A1"
+    }
+    Define.field "SubTitle" {
+      Type = typeof<string>
+      Sheet = "Sheet1"
+      Address = "A2"
+    }
+  ]
---
+++ Sample/Program.fs
@@ -0,0 +1,13 @@
+﻿// F# の詳細については、http://fsharp.net を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+open RealWorlds.ExcelHouganshi.TypeProvider
+
+type MyHouganshi = Houganshi<"Def.fs">
+
+[<EntryPoint>]
+let main argv =
+  let x = MyHouganshi.LoadByNpoi("sample.xls")
+  printfn "%A" x.Title
+  printfn "%A" x.SubTitle
+  0 // 整数の終了コードを返します
